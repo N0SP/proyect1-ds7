cb944d491f650f17293d7707e2e5b1767058060e
asat33 <abidielsoto@gmail.com>
2023-10-23 13:31:28 -0500

    nuevo 2

--- class/navbar.php
+++ class/navbar.php
@@ -1,8 +1,8 @@
 
 <nav>
     <ul>
-        <li><a href="../pagina/index.php">Lista de Tareas</a></li>
-        <li><a href="../pagina/agregar_tarea.php">Agregar Tarea</a></li>
-        <li><a href="../pagina/todas_las_tareas.php">Todas las Tareas</a></li>
+        <li><a href="../agregar_tarea.php">Lista de Tareas</a></li>
+        <li><a href="../agregar_tarea.php">Agregar Tarea</a></li>
+        <li><a href="../todas_las_tareas.php">Todas las Tareas</a></li>
     </ul>
 </nav>
