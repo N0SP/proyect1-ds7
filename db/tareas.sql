0e5a5d2fe061a45c9f4e92d3b3db0aa683e075df
asat33 <abidielsoto@gmail.com>
2023-10-24 22:09:43 -0500

    Update tareas.sql

--- db/tareas.sql
+++ db/tareas.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 22-10-2023 a las 11:51:23
+-- Tiempo de generación: 25-10-2023 a las 05:08:23
 -- Versión del servidor: 10.4.28-MariaDB
 -- Versión de PHP: 8.2.4
 
@@ -20,25 +20,24 @@ SET time_zone = "+00:00";
 --
 -- Base de datos: `tareas`
 --
-CREATE DATABASE IF NOT EXISTS `tareas` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
-USE `tareas`;
 
 DELIMITER $$
 --
 -- Procedimientos
 --
-DROP PROCEDURE IF EXISTS `sp_agregar_tarea`$$
+CREATE DEFINER=`root`@`localhost` PROCEDURE `sp_actualizar_estado` (IN `tarea_id` INT, IN `nuevo_estado` VARCHAR(255))   BEGIN
+    UPDATE tareas SET estado = nuevo_estado WHERE id = tarea_id;
+END$$
+
 CREATE DEFINER=`root`@`localhost` PROCEDURE `sp_agregar_tarea` (IN `p_titulo` VARCHAR(255), IN `p_descripcion` TEXT, IN `p_fecha_compromiso` DATE, IN `p_editada` VARCHAR(3), IN `p_responsable` VARCHAR(255), IN `p_tipo_tarea` VARCHAR(255), IN `p_estado` VARCHAR(255))   BEGIN
     INSERT INTO tareas (titulo, descripcion, fecha_compromiso, editada, responsable, tipo_tarea, estado)
     VALUES (p_titulo, p_descripcion, p_fecha_compromiso, p_editada, p_responsable, p_tipo_tarea, p_estado);
 END$$
 
-DROP PROCEDURE IF EXISTS `sp_eliminar_tarea`$$
 CREATE DEFINER=`root`@`localhost` PROCEDURE `sp_eliminar_tarea` (IN `tarea_id` INT)   BEGIN
     DELETE FROM tareas WHERE id = tarea_id;
 END$$
 
-DROP PROCEDURE IF EXISTS `sp_todos`$$
 CREATE DEFINER=`root`@`localhost` PROCEDURE `sp_todos` ()   BEGIN
     SELECT * FROM tareas;
 END$$
@@ -51,7 +50,6 @@ DELIMITER ;
 -- Estructura de tabla para la tabla `tareas`
 --
 
-DROP TABLE IF EXISTS `tareas`;
 CREATE TABLE `tareas` (
   `id` int(11) NOT NULL,
   `titulo` varchar(255) NOT NULL,
@@ -68,10 +66,11 @@ CREATE TABLE `tareas` (
 --
 
 INSERT INTO `tareas` (`id`, `titulo`, `descripcion`, `fecha_compromiso`, `editada`, `responsable`, `tipo_tarea`, `estado`) VALUES
-(1, 'Tarea 1', 'Descripción tarea 1', '2023-10-18', 'no', 'Juan', 'hogar', 'pendiente'),
-(2, 'Tarea 2', 'Descripción tarea 2', '2023-10-19', 'si', 'María', 'trabajo', 'en_proceso'),
-(3, 'Tarea 3', 'Descripción tarea 3', '2023-10-20', 'no', 'Pedro', 'escuela', 'completa'),
-(10, 'd', 's', '2023-10-04', 'no', 'd', 'hogar', 'pendiente');
+(10, 'd', 's', '2023-10-04', 'no', 'd', 'hogar', 'completa'),
+(11, 's', 's', '2023-10-04', 'no', 'asa', 'hogar', 'completa'),
+(12, 'hola', 'pepe', '2023-10-11', 'no', 'l', 'hogar', 'completa'),
+(13, 's', 'd', '2023-10-19', 'no', 's', 'hogar', 'completa'),
+(14, 'hola', 'd', '2023-10-17', 'no', 'hola', 'hogar', 'completa');
 
 --
 -- Índices para tablas volcadas
@@ -91,7 +90,7 @@ ALTER TABLE `tareas`
 -- AUTO_INCREMENT de la tabla `tareas`
 --
 ALTER TABLE `tareas`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
